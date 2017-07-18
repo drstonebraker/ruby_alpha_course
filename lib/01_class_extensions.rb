c6e770ef2aebeea224bfbb6da6e5399b94780246
drstonebraker <drew.stonebraker@gmail.com>
2017-07-17 21:06:41 -0500

    finish stringify

--- lib/01_class_extensions.rb
+++ lib/01_class_extensions.rb
@@ -104,6 +104,32 @@ end
 
 class Fixnum
   def stringify(base)
+    digits = {
+      0 => '0',
+      1 => '1',
+      2 => '2',
+      3 => '3',
+      4 => '4',
+      5 => '5',
+      6 => '6',
+      7 => '7',
+      8 => '8',
+      9 => '9',
+      10 => 'a',
+      11 => 'b',
+      12 => 'c',
+      13 => 'd',
+      14 => 'e',
+      15 => 'f'
+    }
+
+    result = ""
+    copy = self
+    until copy.zero?
+      copy, mod = copy.divmod(base)
+      result = digits[mod] + result
+    end
+    result
   end
 end
 
