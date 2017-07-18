8908e5b1747d472088ad646bb78722353be6c01e
drstonebraker <drew.stonebraker@gmail.com>
2017-07-17 23:08:30 -0500

    finish myhashset

--- lib/05_my_hash_set.rb
+++ lib/05_my_hash_set.rb
@@ -29,6 +29,53 @@
 # all the items of `set1` that aren't in `set2`.
 
 class MyHashSet
+  def initialize
+    @store = Hash.new(false)
+  end
+
+  def insert(el)
+    @store[el] = true
+  end
+
+  def include?(el)
+    @store[el]
+  end
+
+  def delete(el)
+    @store.delete(el)
+  end
+
+  def to_a
+    @store.keys
+  end
+
+  def union(set2)
+    result = MyHashSet.new
+    fill_result = ->(el) do
+      result.insert(el)
+    end
+    set2.to_a.each(&fill_result)
+    self.to_a.each(&fill_result)
+    result
+  end
+
+  def intersect(set2)
+    result = MyHashSet.new
+    self.to_a.each do |el|
+      result.insert(el) if set2.include?(el)
+    end
+    result
+  end
+
+  def minus(set2)
+    result = MyHashSet.new
+    self.to_a.each do |el|
+      result.insert(el) unless set2.include?(el)
+    end
+    result
+  end
+
+
 end
 
 # Bonus
