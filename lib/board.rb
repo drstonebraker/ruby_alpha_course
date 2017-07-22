093b44edd5cdebc18b496c9b9d76a96d85560ed1
drstonebraker <drew.stonebraker@gmail.com>
2017-07-21 21:48:59 -0500

    pass #winner specs

--- lib/board.rb
+++ lib/board.rb
@@ -24,4 +24,20 @@ class Board
   def empty?(pos)
     self[pos].nil?
   end
+
+  def winner
+    return :X if winner?(:X)
+    return :O if winner?(:O)
+    nil
+  end
+  private
+
+  def winner?(mark)
+    check_rows = ->(row) { row.all?{|space| space == mark } }
+    row_winner = @grid.any?(&check_rows)
+    col_winner = @grid.transpose.any?(&check_rows)
+    d1_winner = [self[[0,0]], self[[1,1]], self[[2,2]]].all? {|space| space == mark }
+    d2_winner = [self[[0,2]], self[[1,1]], self[[2,0]]].all? {|space| space == mark }
+    row_winner || col_winner || d1_winner || d2_winner
+  end
 end
