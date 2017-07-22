9d84aea07e5101afaa0b2a9ce382eee280d4282b
drstonebraker <drew.stonebraker@gmail.com>
2017-07-21 22:03:09 -0500

    pass #get_move specs

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -4,4 +4,10 @@ class HumanPlayer
   def initialize(name)
     @name = name
   end
+
+  def get_move
+    puts 'Please type the location of where you want to move.'
+    puts "Format should be e.g. '0, 0' for top left corner."
+    gets.chomp.split(/\,\s*/).map(&:to_i)
+  end
 end
