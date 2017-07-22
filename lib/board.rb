df3df287984acbdae407d2ba255eb5bcaaa88b1b
drstonebraker <drew.stonebraker@gmail.com>
2017-07-21 21:29:22 -0500

    pass first three specs

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,23 @@
 class Board
+  attr_reader :grid
+
+  def initialize(grid=nil)
+    @grid = grid || [
+      [nil,nil,nil],
+      [nil,nil,nil],
+      [nil,nil,nil]
+    ]
+  end
+
+  def [](pos)
+    row, col = pos
+    @grid[row][col]
+  end
+
+  def []=(pos, mark)
+    row, col = pos
+    @grid[row][col] = mark
+  end
+
+  alias :place_mark :[]=
 end
