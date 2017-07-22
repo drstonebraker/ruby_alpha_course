740ba6e4e64516b51200f384c202578e115b1349
drstonebraker <drew.stonebraker@gmail.com>
2017-07-21 22:41:37 -0500

    pass ComputerPlayer#display specs

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -1,7 +1,11 @@
 class ComputerPlayer
-  attr_reader :name
+  attr_reader :name, :board
 
   def initialize(name=nil)
     @name = name || 'Computer'
+  end
+
+  def display(board)
+    @board = board
   end
 end
