32f78a87453863ea6b4ac27d88454d5f6f148614
drstonebraker <drew.stonebraker@gmail.com>
2017-07-24 06:34:28 -0500

    pass Board::default_grid specs

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -1,3 +1,8 @@
 class BattleshipGame
   attr_reader :board, :player
+
+  def initialize(player, board)
+    @player = player
+    @board = board
+  end
 end
--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,6 @@
 class Board
+
+  def self.default_grid
+    Array.new(10){Array.new(10)}
+  end
 end
