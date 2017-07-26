435b81c026a94f51ea177534e0861209080ab4cc
drstonebraker <drew.stonebraker@gmail.com>
2017-07-25 19:53:45 -0700

    pass ComputerPlayer#check_guess specs

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -8,10 +8,19 @@ end
 class ComputerPlayer
   def initialize(dictionary)
     @dictionary = dictionary
+    @secret_word = @dictionary.sample
+    @guessing_board = Array.new(pick_secret_word)
   end
 
   def pick_secret_word
-    @secret_word = @dictionary.sample
     @secret_word.length
+  end
+
+  def check_guess(guess)
+    matching_idxs = []
+    @secret_word.each_char.with_index do |ch, idx|
+      matching_idxs << idx if guess == ch
+    end
+    matching_idxs
   end
 end
